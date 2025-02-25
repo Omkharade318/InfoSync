c52e42a2d91557dff187966b443c8c90965e66d5
Om kharade <kharadeoms@gmail.com>
2025-02-25 21:58:09 +0530

    Completed NavGraph

--- app/src/main/java/com/example/infosync/MainActivity.kt
+++ app/src/main/java/com/example/infosync/MainActivity.kt
@@ -1,39 +1,33 @@
 package com.example.infosync
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.example.infosync.domain.useCases.AppEntryUseCases
-import com.example.infosync.presentation.onboarding.OnBoardingScreen
-import com.example.infosync.presentation.onboarding.OnBoardingViewModel
+import com.example.infosync.presentation.navigation.NavGraph
 import com.example.infosync.ui.theme.InfoSyncTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.splashCondition
+            }
+        }
 
         enableEdgeToEdge()
         setContent {
             InfoSyncTheme {
-                val viewModel: OnBoardingViewModel = hiltViewModel()
-                OnBoardingScreen(
-                    event =  viewModel :: onEvent
-                )
+                val startDestination = viewModel.startDestination
+                NavGraph(startDestination = startDestination)
             }
         }
     }
---
+++ app/src/main/java/com/example/infosync/presentation/navigation/NavGraph.kt
@@ -0,0 +1,53 @@
+package com.example.infosync.presentation.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.example.infosync.presentation.onboarding.OnBoardingScreen
+import com.example.infosync.presentation.onboarding.OnBoardingViewModel
+
+
+@Composable
+fun NavGraph(
+    startDestination: String,
+){
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ){
+
+        navigation(
+            startDestination = Route.OnBoardingScreen.route,
+            route = Route.AppStartNavigation.route
+        ){
+            composable(route = Route.OnBoardingScreen.route){
+
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(
+                    event =  viewModel :: onEvent
+                )
+
+            }
+        }
+
+        navigation(
+            startDestination = Route.NewsNavigatorScreen.route,
+            route = Route.NewsNavigation.route
+        ){
+            composable(route = Route.NewsNavigatorScreen.route){
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "News Navigator Screen")
+                }
+            }
+        }
+    }
+}
